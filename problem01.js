53089722e184855ef8255e0aae964b7ffa6600f4
km-saifullah <kmsaifullah16@gmail.com>
2023-10-30 11:35:02 +0600

    Problem solving: even odd check and swap two numbers[A[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cping

---
+++ problem01.js
@@ -0,0 +1,9 @@
+// Check the number is even or odd
+let number;
+number = 89;
+
+if (number % 2 == 0) {
+  console.log(`${number} is even!`);
+} else {
+  console.log(`${number} is odd!`);
+}
---
+++ problem02.js
@@ -0,0 +1,10 @@
+// Swap two numbers
+let number1, number2, temp;
+number1 = 10;
+number2 = 20;
+console.log(`Before swap: number1 = ${number1} & number2 = ${number2}`);
+
+temp = number1;
+number1 = number2;
+number2 = temp;
+console.log(`After swap: number1 = ${number1} & number2 = ${number2}`);
