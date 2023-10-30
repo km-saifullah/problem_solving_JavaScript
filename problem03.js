05834f7350ff190f1af4180f09e145849ca3e4fe
km-saifullah <kmsaifullah16@gmail.com>
2023-10-30 11:57:05 +0600

    find out the maximum number

---
+++ problem03.js
@@ -0,0 +1,14 @@
+// Find out the biggest number from three numbers
+
+let number1, number2, number3;
+number1 = 48;
+number2 = 34;
+number3 = 37;
+
+if (number1 > number2 && number1 > number3) {
+  console.log(`${number1} is biggest!`);
+} else if (number2 > number1 && number2 > number3) {
+  console.log(`${number2} is biggest`);
+} else {
+  console.log(`${number3} is biggest`);
+}
